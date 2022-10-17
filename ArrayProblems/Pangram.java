71b2c19706c6367cd772cfe263e5ef7a23b6e117
Ishan-Rawat <rawatishan03@gmail.com>
2022-10-17 22:20:12 +0530

    initial commit and Sentence is Pangram solution

---
+++ ArrayProblems/Pangram.java
@@ -0,0 +1,26 @@
+package ArrayProblems;
+/*
+ * Problem title: 1832. Check if the Sentence Is Pangram
+ * Link: https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+ */
+public class Pangram {
+	public static void main(String args[]) {
+		System.out.println(checker("thequickbrownfoxjumpsoverthelazydog"));
+		System.out.println(checker("bruhmoment"));
+		System.out.println(checker("twodrivenjockshelpfaxmybigquiz"));
+	}
+	//Bruteforce solution:
+	//Depending upon the input I guess its prone to array out of bounds exception
+	static boolean checker(String sentence) {
+		boolean[] arr = new boolean[26];
+		//gotta remember that the decimal value for 'a' in ASCII table is 97 
+		for(int i=0; i<sentence.length();i++) {
+			arr[sentence.charAt(i)%97]=true; //apparently char to int is implicit conversion 
+		}
+		
+		for(int i=0; i<26; i++) {
+			if(arr[i]==false)return false;
+		}
+		return true;
+	}
+}
